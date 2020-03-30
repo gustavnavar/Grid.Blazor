b8999dddeaa9d6ea0c10747ea248a47c5de0efca
Gustavo Navarro <gustav.navar@gmail.com>
2020-03-30 18:03:23 +0200

    Add Blazor WebAssembly 3.2.0-preview3 support

--- GridBlazorClientSide.Server/Startup.cs
+++ GridBlazorClientSide.Server/Startup.cs
@@ -42,15 +42,22 @@ namespace GridBlazorClientSide.Server
                 app.UseDeveloperExceptionPage();
                 app.UseWebAssemblyDebugging();
             }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
-            app.UseStaticFiles();
+            app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
 
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers();
                 endpoints.MapFallbackToFile("index.html");
             });
         }
