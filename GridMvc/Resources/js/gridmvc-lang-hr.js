90e7d4fcea719e161f30395aa4e84308bc82a166
Danijel Dobranovic <danijel.dobranovic@ds-s.hr>
2020-08-13 11:12:35 +0200

    Added Croatian language

---
+++ GridMvc/Resources/js/gridmvc-lang-hr.js
@@ -0,0 +1,29 @@
+/***
+* Grid.Mvc croatian language (hr-HR)
+*/
+window.GridMvc = window.GridMvc || {};
+window.GridMvc.lang = window.GridMvc.lang || {};
+GridMvc.lang.hr = {
+    filterTypeLabel: "Tip:",
+    filterValueLabel: "Vrijednost:",
+    applyFilterButtonText: "Primjeni",
+    filterSelectTypes: {
+        Equals: "Jednako",
+        NotEquals: "Nije Jednako",
+        StartsWith: "Počinje sа",
+        Contains: "Sadrži",
+        EndsWith: "Završava sa",
+        GreaterThan: "Veće od",
+        LessThan: "Manje od",
+        GreaterThanOrEquals: "Veće od ili jednako",
+        LessThanOrEquals: "Manje od ili jednako",
+        And: "i",
+        Or: "ili",
+        IsNull: "Nedefiniran",
+        IsNotNull: "Nije Nedefiniran"
+    },
+    code: 'hr',
+    boolTrueLabel: "Da",
+    boolFalseLabel: "Ne",
+    clearFilterLabel: "Obriši filter"
+};
