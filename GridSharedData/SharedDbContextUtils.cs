0b5810ce759417a377478a80e092aa6ec0a82f56
Robert Giesecke <robert.giesecke@gmail.com>
2020-09-02 22:59:57 +0200

    replace SqlClient with NpgSql - supports snake-case database names

--- GridSharedData/SharedDbContextUtils.cs
+++ GridSharedData/SharedDbContextUtils.cs
@@ -2,21 +2,36 @@
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Npgsql;
+using Npgsql.NameTranslation;
 
 namespace GridShared.Data
 {
     public static class SharedDbContextUtils
     {
-        public static string ConnectionString = "Server=.\\SQLEXPRESS;Database=NorthWind;Trusted_Connection=True;MultipleActiveResultSets=true";
-
         public static DbContextOptionsBuilder UseGridBlazorDatabase(this DbContextOptionsBuilder optionsBuilder)
         {
-            return optionsBuilder.UseSqlServer(ConnectionString);
+            return optionsBuilder.UseNpgsql(new NpgsqlConnectionStringBuilder()
+            {
+                Host = "localhost",
+                Database = "northwind",
+                SearchPath = "northwind",
+                Username = "postgres",
+            }.ToString());
         }
         
         public static void ApplyToModelBuilder(DatabaseFacade databaseFacade, ModelBuilder modelBuilder)
         {
-            
+            var mapper = new NpgsqlSnakeCaseNameTranslator();
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                foreach (var property in entity.GetProperties())
+                {
+                    property.SetColumnName(mapper.TranslateMemberName(property.GetColumnName()));
+                }
+
+                entity.SetTableName(mapper.TranslateTypeName(entity.GetTableName()));
+            }
         }
     }
 }
