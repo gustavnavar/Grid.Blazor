8808a2dd372c3fee70e7ba5acc4e82ecf1a73cbf
Robert Giesecke <robert.giesecke@gmail.com>
2020-09-02 20:52:28 +0200

    new project GridSharedData

---
+++ GridSharedData/SharedDbContextUtils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace GridShared.Data
+{
+    public static class SharedDbContextUtils
+    {
+        public static void ApplyToModelBuilder(DatabaseFacade databaseFacade, ModelBuilder modelBuilder)
+        {
+            
+        }
+    }
+}
