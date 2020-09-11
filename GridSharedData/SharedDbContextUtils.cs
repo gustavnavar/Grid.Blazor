58d183db8af3ae0981e266c6bd535dc0471c3f86
Gustau Navarro <gn@agno.me>
2020-09-11 16:01:22 +0200

    Add a switch to use SqlServer or Sqlite database for samples

--- GridSharedData/SharedDbContextUtils.cs
+++ GridSharedData/SharedDbContextUtils.cs
@@ -1,43 +1,66 @@
-﻿using System;
-using System.IO;
-using System.Linq;
-using Microsoft.Data.Sqlite;
+﻿using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Npgsql.NameTranslation;
+using System.IO;
 
 namespace GridShared.Data
 {
     public static class SharedDbContextUtils
     {
+        public static DbProvider DbProvider = DbProvider.Sqlite;
+
+        public static string ConnectionString = "Server=.\\SQLEXPRESS;Database=NorthWind;Trusted_Connection=True;MultipleActiveResultSets=true";
+
         public static DbContextOptionsBuilder UseGridBlazorDatabase(this DbContextOptionsBuilder optionsBuilder)
         {
-            var northwindDb = "../northwind.db";
-            if (!File.Exists(northwindDb))
+            switch (DbProvider)
             {
-                using var resxStream = typeof(SharedDbContextUtils).Assembly.GetManifestResourceStream(
-                    $"{typeof(SharedDbContextUtils).Namespace}.northwind.db");
-                using var fs = new FileStream(northwindDb, FileMode.Create, FileAccess.ReadWrite);
-                resxStream.CopyTo(fs);
+                case DbProvider.SqlServer:
+                    return optionsBuilder.UseSqlServer(ConnectionString);
+                case DbProvider.Sqlite:
+                default:
+                    var northwindDb = "../northwind.db";
+                    if (!File.Exists(northwindDb))
+                    {
+                        using var resxStream = typeof(SharedDbContextUtils).Assembly.GetManifestResourceStream(
+                            $"{typeof(SharedDbContextUtils).Namespace}.northwind.db");
+                        using var fs = new FileStream(northwindDb, FileMode.Create, FileAccess.ReadWrite);
+                        resxStream.CopyTo(fs);
+                    }
+                    return optionsBuilder.UseSqlite(new SqliteConnectionStringBuilder
+                    {
+                        DataSource = northwindDb,
+                    }.ToString());
             }
-            return optionsBuilder.UseSqlite(new SqliteConnectionStringBuilder
-            {
-                DataSource = northwindDb,
-            }.ToString());
         }
         
         public static void ApplyToModelBuilder(DatabaseFacade databaseFacade, ModelBuilder modelBuilder)
         {
-            var mapper = new NpgsqlSnakeCaseNameTranslator();
-            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            switch (DbProvider)
             {
-                foreach (var property in entity.GetProperties())
-                {
-                    property.SetColumnName(mapper.TranslateMemberName(property.GetColumnName()));
-                }
+                case DbProvider.SqlServer:
+                    break;
+                case DbProvider.Sqlite:
+                default:
+                    var mapper = new NpgsqlSnakeCaseNameTranslator();
+                    foreach (var entity in modelBuilder.Model.GetEntityTypes())
+                    {
+                        foreach (var property in entity.GetProperties())
+                        {
+                            property.SetColumnName(mapper.TranslateMemberName(property.GetColumnName()));
+                        }
 
-                entity.SetTableName(mapper.TranslateTypeName(entity.GetTableName()));
+                        entity.SetTableName(mapper.TranslateTypeName(entity.GetTableName()));
+                    }
+                    break;
             }
         }
+    }
+
+    public enum DbProvider
+    {
+        SqlServer,
+        Sqlite
     }
 }
