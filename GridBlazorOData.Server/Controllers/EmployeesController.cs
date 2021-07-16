b8688b938892ba7a009a8e1e27e7be53f19f04ec
Gustau Navarro <gn@agno.me>
2021-07-16 15:51:01 +0200

    Update Odata sample with AspNetCoreOData 8.0.1

--- GridBlazorOData.Server/Controllers/EmployeesController.cs
+++ GridBlazorOData.Server/Controllers/EmployeesController.cs
@@ -1,8 +1,8 @@
 ﻿using GridBlazorOData.Server.Models;
 using GridBlazorOData.Shared.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OData.Deltas;
 using Microsoft.AspNetCore.OData.Formatter;
-using Microsoft.AspNetCore.OData.Formatter.Value;
 using Microsoft.AspNetCore.OData.Query;
 using Microsoft.AspNetCore.OData.Routing.Controllers;
 using Microsoft.EntityFrameworkCore;
