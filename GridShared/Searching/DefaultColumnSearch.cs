e85a9b4a967520218a4ac69d67743eecb360ce79
Gustau Navarro <gn@agno.me>
2025-02-28 12:05:12 +0100

    Update package 6.0.5

--- GridShared/Searching/DefaultColumnSearch.cs
+++ GridShared/Searching/DefaultColumnSearch.cs
@@ -44,6 +44,8 @@ namespace GridShared.Searching
                 // bool columns are not searched as a workaround until the final release of EF Core 3.0
                 if (targetType == typeof(bool)) return null;
 
+                if (targetType == typeof(Enum) || targetType.BaseType == typeof(Enum)) return null;
+
                 if (onlyTextColumns && targetType != typeof(string))
                     return null;
 
